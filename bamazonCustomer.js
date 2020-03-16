84ad485e17e7e900e34d958edb8c5b94b316a935
Miranda D'Asto <merlinockipella@gmail.com>
2020-03-16 16:06:48 -0600

    Created runBamazon function which asks the customer what the id of the item is that they want then asks how many they want. Sends responses to the purchaseItems function which calculates total and subtracts wanted amounts from stock total in database. If wanted amounts are not available, it says so.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,9 +27,61 @@ function readProducts()
    console.log("The following items are available products:\n");
    connection.query("SELECT * FROM products", function(err, res)
    {
-       if (err) throw err;;
+       if (err) throw err;
 
        console.log(res);
-       connection.end()
-   })
+       runBamazon();
+    })
+}
+
+function runBamazon()
+{
+    inquirer.prompt([
+        {
+            type: "number",
+            message: "What is the ID of the item you would like to purchase?",
+            name: "Id"
+        },
+        {
+            type: "number",
+            message: "How many units of this product would you like to buy?",
+            name: "Quantity"
+        }
+    ])
+    .then(function(response)
+    {
+        var itemWanted = response.Id;
+        var quantityRequested = response.Quantity;
+        
+        purchaseItems(itemWanted, quantityRequested);
+    })
+    .catch(function(error)
+    {
+        if (error) throw error;
+    })
+};
+
+function purchaseItems(ID, quantity)
+{
+    connection.query('SELECT * FROM products WHERE id = ' + ID, function(err, res)
+    {
+        if (err)
+        {
+            console.log(err);
+        }
+        else if (quantity <= res[0].stock_quantity)
+        {
+            var total = res[0].price * quantity;
+
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + quantity + " WHERE id = " + ID);
+
+            console.log(res[0].stock_quantity);
+
+            console.log("The total for your purchase of " + quantity + " " + res[0].product_name + " is " + total + ". Thank you for shopping with Bamazon!")
+        }
+        else
+        {
+            console.log("We do not have that many units of this item to sell you. We only have " + res[0].stock_quantity + " units available at this time.");
+        };
+    })
 }
